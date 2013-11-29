c72b1a2517975fa2aa3dc26205d5cb3a894bd456
l.hollingworth1 <l.hollingworth1>
2013-11-29 18:30:46 +0000

    Added button function for Quiz button sort of!
    Signed-off-by: l.hollingworth1 <l.hollingworth1>

--- NewCode/src/turtle/app/antibully/QuizStarterActivity.java
+++ NewCode/src/turtle/app/antibully/QuizStarterActivity.java
@@ -2,20 +2,25 @@ package turtle.app.antibully;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizStarterActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_quiz_starter);
-		
-		Button startButton = (Button) findViewById(R.id.button3);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_starter);
+
+        //setup button listener
+        Button startButton = (Button) findViewById(R.id.button1);
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-            finish();
-	}
-}
+                finish();
+            }
+        });
+    }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
