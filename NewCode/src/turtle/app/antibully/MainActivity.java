2f15db57dd0d51d6f860000a11ad72e09e1a3cb9
TheBornTurtle <bornturtle@outlook.com>
2013-12-02 20:14:39 +0000

    Updated so there is a new page that is working!

--- NewCode/src/turtle/app/antibully/MainActivity.java
+++ NewCode/src/turtle/app/antibully/MainActivity.java
@@ -1,35 +1,32 @@
 package turtle.app.antibully;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);Button startQuiz = (Button) findViewById(R.id.button1);
-		startQuiz.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "Transferring to Quiz page!", Toast.LENGTH_LONG).show();
-				startActivity(new Intent(MainActivity.this, QuizStarterActivity.class));
-				
-			}
-		});
+	 Button button1;
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.activity_main);
+		 addListenerOnButton();
+	 }
+	 
+	 public void addListenerOnButton() {
+		 final Context context = this;
+		 button1 = (Button) findViewById(R.id.button1);
+		 button1.setOnClickListener(new OnClickListener() {
+			 @Override
+			 public void onClick(View arg0) {
+				 Intent intentq = new Intent(context, QuizStarterActivity.class);
+				 startActivity(intentq);
+			 }
+		 });
+	 }
+	 
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
--- NewCode/src/turtle/app/antibully/QuizStarterActivity.java
+++ NewCode/src/turtle/app/antibully/QuizStarterActivity.java
@@ -1,31 +1,36 @@
 package turtle.app.antibully;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class QuizStarterActivity extends Activity {
 
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_starter);
+        Intent intent = getIntent();
 
-        //setup button listener
-        Button startButton = (Button) findViewById(R.id.button1);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.quiz_starter, menu);
-		return true;
-	}
-}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
